822a835bebc1b3782b252427100eb2d65b65d905
Jose-Prince-Barwa <josebarwa2020@gmail.com>
2025-10-28 23:55:48 +0530

    hierarchy program

---
+++ Hierarchy.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Hierarchy{
+
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+        int pop_n, p_pal, dean, prof, stu;
+        System.out.println("Enter the population :");
+        pop_n = sc.nextInt();
+
+        dean = (pop_n - 1)/106;
+        prof = 5 * dean;
+        stu =  20 * dean;
+
+        System.out.println("Principal :1");
+        System.out.println("Deans :"+dean);
+        System.out.println("Professors :"+prof);
+        System.out.println("Students :"+stu);
+    }
+}

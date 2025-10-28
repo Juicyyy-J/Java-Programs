07cf41f496664e23fc6994bc79f7e9fbd977de82
Jose-Prince-Barwa <josebarwa2020@gmail.com>
2025-10-28 23:57:43 +0530

    close

--- Hierarchy.java
+++ Hierarchy.java
@@ -4,7 +4,7 @@ class Hierarchy{
 
     public static void main(String[] args) {
         Scanner sc =  new Scanner(System.in);
-        int pop_n, p_pal, dean, prof, stu;
+        int pop_n, dean, prof, stu;
         System.out.println("Enter the population :");
         pop_n = sc.nextInt();
 
@@ -16,5 +16,7 @@ class Hierarchy{
         System.out.println("Deans :"+dean);
         System.out.println("Professors :"+prof);
         System.out.println("Students :"+stu);
+
+        sc.close();
     }
 }
